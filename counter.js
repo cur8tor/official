12f322cfad08dc0147c9457abb9e5e2211cddd8d
The Curator <tmartzdc@gmail.com>
2020-04-16 11:15:00 -0400

    n

--- counter.js
+++ counter.js
@@ -1,14 +1,14 @@
 window.onload = getArrowFunction;
 var xtext=0;
 function upArrowFunction() {
-  if (xtext==6) {
+  if (xtext>=6) {
     xtext=0;
   } else {
     xtext+=1;
   }
 }
 function downArrowFunction() {
-  if (xtext==0) {
+  if (xtext<=0) {
     xtext=6;
   } else {
     xtext-=1;
@@ -16,28 +16,30 @@ function downArrowFunction() {
 }
 function getArrowFunction() {
   if (xtext==0){
-    document.getElementById("demo").innerHTML = "give";
     document.getElementById("link").href = "give.html";
+    //document.getElementById("box").style.backgroundImage = "url('giveH3.png')";
+    document.getElementById("imgX").src = "giveH3.png";
   } else if (xtext==1){
-    document.getElementById("demo").innerHTML = "photography";
     document.getElementById("link").href = "photography.htm";
+    document.getElementById("imgX").src = "photoH3.png";
   } else if (xtext==2){
-    document.getElementById("demo").innerHTML = "games";
     document.getElementById("link").href = "arcade.htm";
+    //ssdocument.getElementById("box").style.backgroundImage = "url('david2.jpg')";
+    document.getElementById("imgX").src = "arcadeH3.png";
   } else if (xtext==3){
-    document.getElementById("demo").innerHTML = "fashion";
     document.getElementById("link").href = "fash.htm";
+    document.getElementById("imgX").src = "fashH3.png";
   } else if (xtext==4){
-    document.getElementById("demo").innerHTML = "music";
     document.getElementById("link").href = "music.htm";
+    document.getElementById("imgX").src = "musicH3.png";
   } else if (xtext==5){
-    document.getElementById("demo").innerHTML = "film";
     document.getElementById("link").href = "film.htm";
+    document.getElementById("imgX").src = "filmH3.png";
   } else if (xtext==6){
-    document.getElementById("demo").innerHTML = "magazine";
     document.getElementById("link").href = "mag.htm";
+    document.getElementById("imgX").src = "magH3.png";
   } else {
-    document.getElementById("demo").innerHTML = "*error*";
     document.getElementById("link").href = "index.htm";
+    document.getElementById("imgX").src = "errorH3.png";
   }
 }
