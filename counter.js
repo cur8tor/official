b985b9158cf74569207414e48e643cb4fa82c88b
The Curator <tmartzdc@gmail.com>
2020-05-19 23:04:26 -0400

    m

--- counter.js
+++ counter.js
@@ -1,7 +1,8 @@
 window.onload = getArrowFunction;
 var xtext=0;
+
 function upArrowFunction() {
-  if (xtext>=6) {
+  if (xtext>=7) {
     xtext=0;
   } else {
     xtext+=1;
@@ -9,7 +10,7 @@ function upArrowFunction() {
 }
 function downArrowFunction() {
   if (xtext<=0) {
-    xtext=6;
+    xtext=7;
   } else {
     xtext-=1;
   }
@@ -17,27 +18,28 @@ function downArrowFunction() {
 function getArrowFunction() {
   if (xtext==0){
     document.getElementById("link").href = "give.html";
-    //document.getElementById("box").style.backgroundImage = "url('giveH3.png')";
     document.getElementById("imgX").src = "giveH3.png";
   } else if (xtext==1){
-    document.getElementById("link").href = "photography.htm";
-    document.getElementById("imgX").src = "photoH3.png";
-  } else if (xtext==2){
-    document.getElementById("link").href = "arcade.htm";
-    //ssdocument.getElementById("box").style.backgroundImage = "url('david2.jpg')";
-    document.getElementById("imgX").src = "arcadeH3.png";
-  } else if (xtext==3){
-    document.getElementById("link").href = "fash.htm";
-    document.getElementById("imgX").src = "fashH3.png";
-  } else if (xtext==4){
-    document.getElementById("link").href = "music.htm";
-    document.getElementById("imgX").src = "musicH3.png";
-  } else if (xtext==5){
-    document.getElementById("link").href = "film.htm";
-    document.getElementById("imgX").src = "filmH3.png";
-  } else if (xtext==6){
     document.getElementById("link").href = "mag.htm";
-    document.getElementById("imgX").src = "magH3.png";
+    document.getElementById("imgX").src = "moduleTop3.png";
+  } else if (xtext==2){
+    document.getElementById("link").href = "mag.htm";
+    document.getElementById("imgX").src = "moduleYT.png";
+  } else if (xtext==3){
+    document.getElementById("link").href = "photography.htm";
+    document.getElementById("imgX").src = "modulePhoto.png";
+  } else if (xtext==4){
+    document.getElementById("link").href = "https://www.instagram.com/curator.vision/";
+    document.getElementById("imgX").src = "moduleInsta.png";
+  } else if (xtext==5){
+    document.getElementById("link").href = "music.htm";
+    document.getElementById("imgX").src = "modSpot.png";
+  } else if (xtext==6){
+    document.getElementById("link").href = "arcade.htm";
+    document.getElementById("imgX").src = "moduleArc.png";
+  } else if (xtext==7){
+    document.getElementById("link").href = "fash.htm";
+    document.getElementById("imgX").src = "moduleShop.png";
   } else {
     document.getElementById("link").href = "index.htm";
     document.getElementById("imgX").src = "errorH3.png";
